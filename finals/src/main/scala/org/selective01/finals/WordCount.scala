c7154376a32ea80e82ec36095659ea491295c138
jerx <jerxdev@alienetworx.com>
2016-10-02 06:45:48 +0800

    first commit

---
+++ finals/src/main/scala/org/selective01/finals/WordCount.scala
@@ -0,0 +1,31 @@
+package org.selective01.finals
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  
+  def main(args: Array[String]) = {
+    
+    System.setProperty("hadoop.home.dir", "C:\\winutil\\");
+    
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+    
+    val inputfile = sc.textFile("input.txt")
+    inputfile.flatMap (_.split(" "))
+    .filter(x => x.matches("[a-zA-Z-0-9]+"))
+    
+    .map { word =>
+      ( word , 1 )
+     
+    }
+    
+    .reduceByKey(_ + _)
+    .saveAsTextFile("input.result.txt")
+    
+    sc.stop
+  }
+}
